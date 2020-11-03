dd6cb7fe48bb8d3409aa11bac9db0489e4ab35d1
hidems <hidemiura.arms@gmail.com>
2020-11-03 01:56:36 +0100

    fix a bug to redirect after posting

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -19,12 +19,12 @@ class PostsController extends Controller
     public function store()
     {
         $attributes = request()->validate(['body' => 'required|max:255']);
-    
+
         Post::create([
             'user_id' => auth()->id(),
             'body' => $attributes['body']
         ]);
-    
-        return redirect('/posts');
+
+        return redirect(route('home'));
     }
 }
