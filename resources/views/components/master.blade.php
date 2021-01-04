811f9521d626d5d74cc4ebf823089d49fab4a4e0
hidems <hidemiura.arms@gmail.com>
2021-01-04 11:22:39 +0100

    add site description

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="description" content="Sie können sich hier über Japan austauschen. Vielleicht kennen Sie sich auch gut in Japan aus. Dann können anderen Leuten helfen, indem Sie deren Fragen beantworten.">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
