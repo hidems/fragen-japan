f856309f6fc11bd98f082d590b47efb1c1d2c8bb
hidems <hidemiura.arms@gmail.com>
2021-02-06 16:06:02 +0100

    complete to implement vue and spmenu by vue

--- public/js/sp-menu.js
+++ public/js/sp-menu.js
@@ -1,5 +1,13 @@
+// jQuery
 // $(function(){
 //     $('.menu-btn').on('click', function(){
 //         $('.menu').toggleClass('active');
 //     });
 // }());
+
+var vm = new Vue({
+	el: '#vue',
+	data: {
+		isActive: false,
+	}
+})
