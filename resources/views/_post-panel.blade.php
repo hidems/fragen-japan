e16a6387846f9bb92d3dcbdbcb0a0094c55a4956
hidems <miura.eleven@gmail.com>
2021-03-17 21:40:35 +0100

    Add func to expand textarea by Line break

--- resources/views/_post-panel.blade.php
+++ resources/views/_post-panel.blade.php
@@ -8,6 +8,7 @@
             class="w-full"
             placeholder="Was ist Ihre Frage über Japan?"
             v-model.trim="postPanelText"
+            :rows="postPanelText.split(/\n|\r\n|\r/).length + 1"
             required
         >{{ old('body') }}</textarea>
 
