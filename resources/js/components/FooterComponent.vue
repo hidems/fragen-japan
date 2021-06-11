5e5590f0c389ae554fa5e757018bed3f87f20cdb
hidems <miura.eleven@gmail.com>
2021-06-11 23:36:38 +0200

    Add new file of components, Footer and Timeline

---
+++ resources/js/components/FooterComponent.vue
@@ -0,0 +1,9 @@
+<template>
+<footer class="py-2 bg-gray-600">
+    <p class="text-center text-xs text-white">Copyright © 2020 hidms All Rights Reserved.</p>
+</footer>
+</template>
+
+<script>
+    export default {}
+</script>
---
+++ resources/js/components/TimelineComponent.vue
@@ -0,0 +1,24 @@
+<template>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                posts: []
+            }
+        },
+        methods: {
+            getPosts() {
+                axios.get('/api/posts')
+                    .then((res) => {
+                        this.posts = res.data;
+                        console.log(this.posts);
+                    });
+            }
+        },
+        mounted() {
+            this.getPosts();
+        }
+    }
+</script>
