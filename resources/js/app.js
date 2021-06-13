47d920882d6ef6aee4ee90ccc5f28c6962ec6a22
hidems <miura.eleven@gmail.com>
2021-06-13 16:02:13 +0200

    Fix resources/js/app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@
  */
 
 import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
+import TimelineComponent from "./components/TimelineComponent";
+
 
 require('./bootstrap');
 
@@ -21,8 +24,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('header-component', HeaderComponent);
+Vue.component('footer-component', FooterComponent);
+Vue.component('timeline-component', TimelineComponent);
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
